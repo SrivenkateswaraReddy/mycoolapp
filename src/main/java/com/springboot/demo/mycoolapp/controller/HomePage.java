a7a3cec166ce0bebe1606c4aae8d94a79c63074e
SrivenkateswaraReddy <venki.sankiti@gmail.com>
2023-06-19 12:54:57 -0500

    added new changes and removed swagger integration

--- src/main/java/com/springboot/demo/mycoolapp/controller/HomePage.java
+++ src/main/java/com/springboot/demo/mycoolapp/controller/HomePage.java
@@ -1,5 +1,6 @@
 package com.springboot.demo.mycoolapp.controller;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,6 +21,7 @@ public class HomePage {
 //    }
 
     // expose a new endpoint for workout
+
 
     @GetMapping("/workout")
     public String getDailyWorkout() {
